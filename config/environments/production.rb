8a6551be00bde66420eaa7b1722327100adf5fbd
unknown <saginalamasthan@gmail.com>
2021-09-30 16:39:28 +0530

    configuration added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'https://sleepy-ocean-07188.herokuapp.com//' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+:address              => "smtp.gmail.com",
+:port                 => 587,
+:user_name            => "saginala1234@gmail.com",
+:password             => 'Masthan@2021',
+:authentication       => "plain",
+:enable_starttls_auto => true
+
 end
